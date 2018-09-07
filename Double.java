8528f397231206acf2719d2ae9c8f99d1061b662
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-09-07 12:30:29 +0530

    Add files via upload

---
+++ Double.java
@@ -0,0 +1,12 @@
+class Double {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e,f;
+    a=250;b=250.250f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println(c+" "+d+" "+e+" "+f);
+  }
+}
