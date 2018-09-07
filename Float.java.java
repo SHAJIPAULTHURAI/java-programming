bc1e9dcf8d12485e8448cf15b7d171683a4d38d7
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-09-07 12:12:09 +0530

    Add files via upload

---
+++ Float.java.java
@@ -0,0 +1,11 @@
+class Float {
+  public static void main(String[] args) {
+    int a;
+    double b,c,d,e;
+    a=250;b=250.250;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    System.out.println(c+" "+d+" "+e);
+  }
+}
---
+++ Main.java.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e;
+    a=30;b=20;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    System.out.println(c+" "+d+" "+e);
+  }
+}
