69da5bc7674d6770f98a8f6f1307aa8dc6677dd3
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-01 15:46:17 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Data
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String name="Shaji";
+        String city="Chennai";
+        int age=21;
+        float height=5.2f;
+        boolean married=false;
+        char sex='F';
+        double city_population=5026846.00;
+        System.out.println("Name:" +name + "\n City:" +city + "\n Age:" +age + "\n Height:"+height +"\n Married:" +married +"\n Sex:" +sex +"\n City Population:" + city_population);	// your code goes here
+	}
+}
