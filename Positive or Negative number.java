618d4f94aa80b380651253e3e75387817548637f
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-03 12:14:02 +0530

    Update Positive or Negative number.java

--- Positive or Negative number.java
+++ Positive or Negative number.java
@@ -1,13 +1,17 @@
 class Main {
   public static void main(String[] args) {
-     int number=10;
+    int number=5;
         if(number > 0)
         {
-            System.out.println(number+" is a positive number");
+            System.out.println("Positive");
         }
         else if(number < 0)
         {
-            System.out.println(number+" is a negative number");
-        }   
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
     }
 }
