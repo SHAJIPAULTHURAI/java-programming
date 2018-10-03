9d053b2b0e6f9a7d5ba12f20cf27db46f3239720
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-03 12:39:42 +0530

    Create Odd or Even.java

---
+++ Odd or Even.java
@@ -0,0 +1,18 @@
+class Class {
+  public static void main(String[] args) {
+    int i=467;
+
+for (int a =i;a<=i; a++)
+{
+
+    if (a % 2 == 0) 
+    {
+        System.out.println("Even");
+    }
+    else
+    {
+        System.out.println("Odd");
+    }
+}
+    }
+}
