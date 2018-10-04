67f0d9642ea99fb5a1d815e2c85615fdfb23e26e
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-04 12:34:28 +0530

    Create Alphabet_not.java

---
+++ Alphabet_not.java
@@ -0,0 +1,9 @@
+class Alphabet_not{
+  public static void main (String[] args){
+     char c = 'a';
+        if( (c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+            System.out.println("Alphabet");
+        else
+            System.out.println("Not an Alphabet");  
+      }
+ }
