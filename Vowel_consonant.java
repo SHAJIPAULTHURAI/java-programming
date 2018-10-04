aa2e3c55b7a2e70118d3ff91aef18117c5cc35d6
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-04 12:02:02 +0530

    Update Vowel_consonant.java

--- Vowel_consonant.java
+++ Vowel_consonant.java
@@ -4,9 +4,17 @@ class Vowelorconsonant{
        switch(ch)
        {
        case 'a':
+           System.out.println("Vowel");
+       break;
        case 'e':
+           System.out.println("Vowel");
+       break;
        case 'i':
+           System.out.println("Vowel");
+       break;
        case 'o':
+           System.out.println("Vowel");
+       break;
        case 'u':
        System.out.println("Vowel");
        break;
