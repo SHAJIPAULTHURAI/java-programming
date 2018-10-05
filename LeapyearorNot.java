a4a1d38d6530d88d84bdb54ec41b6b91a3ea7823
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-05 12:36:36 +0530

    Create LeapyearorNot.java

---
+++ LeapyearorNot.java
@@ -0,0 +1,13 @@
+class LeapyearorNot {
+  public static void main(String[] args){
+int Year = 2000; 
+         if (Year %4 == 0) 
+            {
+                System.out.print("yes");
+            }
+         else
+            {
+               System.out.println("no");
+            }
+}
+}
