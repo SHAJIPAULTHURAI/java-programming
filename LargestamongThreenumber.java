b249e0d0273a93ee6bfc39f836c10658e512e6fc
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-05 12:10:00 +0530

    Update LargestamongThreenumber.java

--- LargestamongThreenumber.java
+++ LargestamongThreenumber.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-Class LargestamongThreenumber{
-public static void main(String[] arg){
-int a,b,c;
-System.out.println("Enter three numbers");
-Scanner in=new Scanner(System.in);
+Class LargestamongThreenumber {
+public static void main(String arg[]) {
+    int a,b,c;
+         System.out.println("Enter three numbers");
+    Scanner in=new Scanner(System.in);
 a=in.nextInt();
 b=in.nextInt();
 c=in.nextInt();
-if(a > b && a > c)
-System.out.println(a);
-else if(b > a && b>c)
-System.out.println(b);
-else
-System.out.println(c);
-}
+     if(a > b && a > c)
+         System.out.println(a);
+    else if(b > a && b > c)
+         System.out.println(b);
+    else
+          System.out.println(c);
+     }
 }
 
