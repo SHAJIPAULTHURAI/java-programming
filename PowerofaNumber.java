bd32ebd78b00186e4b28a44d7366ac588c18b46c
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-08 22:27:20 +0530

    Create PowerofaNumber.java

---
+++ PowerofaNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PowerofaNumber {
+
+  public static void main(String[] args) {
+        int base, exponent;
+
+        long result = 1;
+  System.out.println("");
+   Scanner s = new Scanner(System.in);
+base=s.nextInt();
+exponent=s.nextInt();
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
