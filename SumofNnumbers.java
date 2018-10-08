568915be2f1cf55716b2e8372d1af835dcab9548
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-08 22:04:34 +0530

    Update SumofNnumbers.java

--- SumofNnumbers.java
+++ SumofNnumbers.java
@@ -3,9 +3,9 @@ class SumofNnumbers {
   public static void main(String[] args) {
    int a,i=1;
    int b=0;
+   System.out.println("");
    Scanner s = new Scanner(System.in);
    a=s.nextInt();
-   System.out.println(a);
    while(i<=a)
    {
      b=b+i;
