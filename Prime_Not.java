6deb0da6d8e789edcfcf271755ff105fe0f7d21c
SHAJIPAULTHURAI <43032357+SHAJIPAULTHURAI@users.noreply.github.com>
2018-10-09 23:34:07 +0530

    Create Prime_Not.java

---
+++ Prime_Not.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Prime_Not {
+  public static void main(String[] args) {
+    int a;
+    System.out.println("");
+    Scanner s = new Scanner(System.in);
+    a = s.nextInt();
+        boolean flag = false;
+        for(int i = 2; i <= a/2; ++i)
+        {
+            if(a % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println(" is a prime number.");
+        else
+            System.out.println(" is not a prime number.");
+    }
+}
+  
